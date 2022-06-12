09efc7cce0d64f8bbe5765616b5106805b08037e
Good Do <56465252+genegods@users.noreply.github.com>
2022-06-12 17:32:58 +0800

    Add files via upload

---
+++ nestedGrid/src/components/MainPage.js
@@ -0,0 +1,307 @@
+import React from 'react'
+import { DivisionData } from '../data/DivisionData'
+
+const MainPage = () => {
+  return (
+    <React.Fragment>
+    <section className='uppercase grid grid-cols-3 gap-1 mx-1 md:grid-cols-4 md:w-2/5 md:mx-auto'>
+      <div className="grid grid-cols-4 h-40 w-40 font-bold text-white ">
+          {
+              DivisionData.map((item,index) =>{
+                  return(
+                      <div key={index} className={`centered ${item.color}`}>
+                          <div className='w-8 h-8 centered'>
+                              <span>{item.number}</span>
+                          </div>
+                      </div>
+                  )
+              })
+          }
+      </div>
+
+      <div className="grid grid-cols-4 h-40 w-40 font-bold text-black">
+          {
+              DivisionData.map((item,index) =>{
+                  return(
+                      <div key={index} className={`centered ${item.color}`}>
+                          <div className='w-8 h-8 centered'>
+                              <span>{item.alphabet}</span>
+                          </div>
+                      </div>
+                  )
+              })
+          }
+      </div>
+
+      <div className="grid grid-cols-4 h-40 w-40 font-bold text-white ">
+          {
+              DivisionData.map((item,index) =>{
+                  return(
+                      <div key={index} className={`centered ${item.color}`}>
+                          <div className='w-8 h-8 centered'>
+                              <span>{item.number}</span>
+                          </div>
+                      </div>
+                  )
+              })
+          }
+      </div>
+
+      <div className="grid grid-cols-4 h-40 w-40 font-bold text-black">
+          {
+              DivisionData.map((item,index) =>{
+                  return(
+                      <div key={index} className={`centered ${item.color}`}>
+                          <div className='w-8 h-8 centered'>
+                              <span>{item.alphabet}</span>
+                          </div>
+                      </div>
+                  )
+              })
+          }
+      </div>
+
+
+      <div className="grid grid-cols-4 h-40 w-40 font-bold text-white ">
+          {
+              DivisionData.map((item,index) =>{
+                  return(
+                      <div key={index} className={`centered ${item.color}`}>
+                          <div className='w-8 h-8 centered'>
+                              <span>{item.number}</span>
+                          </div>
+                      </div>
+                  )
+              })
+          }
+      </div>
+
+      <div className="grid grid-cols-4 h-40 w-40 font-bold text-black">
+          {
+              DivisionData.map((item,index) =>{
+                  return(
+                      <div key={index} className={`centered ${item.color}`}>
+                          <div className='w-8 h-8 centered'>
+                              <span>{item.alphabet}</span>
+                          </div>
+                      </div>
+                  )
+              })
+          }
+      </div>
+
+
+      <div className="grid grid-cols-4 h-40 w-40 font-bold text-white ">
+          {
+              DivisionData.map((item,index) =>{
+                  return(
+                      <div key={index} className={`centered ${item.color}`}>
+                          <div className='w-8 h-8 centered'>
+                              <span>{item.number}</span>
+                          </div>
+                      </div>
+                  )
+              })
+          }
+      </div>
+
+      <div className="grid grid-cols-4 h-40 w-40 font-bold text-black">
+          {
+              DivisionData.map((item,index) =>{
+                  return(
+                      <div key={index} className={`centered ${item.color}`}>
+                          <div className='w-8 h-8 centered'>
+                              <span>{item.alphabet}</span>
+                          </div>
+                      </div>
+                  )
+              })
+          }
+      </div>
+
+      
+
+      <div className="grid grid-cols-4 h-40 w-40 font-bold text-white ">
+          {
+              DivisionData.map((item,index) =>{
+                  return(
+                      <div key={index} className={`centered ${item.color}`}>
+                          <div className='w-8 h-8 centered'>
+                              <span>{item.number}</span>
+                          </div>
+                      </div>
+                  )
+              })
+          }
+      </div>
+
+
+      
+      <div className="grid grid-cols-4 h-40 w-40 font-bold text-black">
+          {
+              DivisionData.map((item,index) =>{
+                  return(
+                      <div key={index} className={`centered ${item.color}`}>
+                          <div className='w-8 h-8 centered'>
+                              <span>{item.alphabet}</span>
+                          </div>
+                      </div>
+                  )
+              })
+          }
+      </div>
+
+
+      <div className="grid grid-cols-4 h-40 w-40 font-bold text-white ">
+          {
+              DivisionData.map((item,index) =>{
+                  return(
+                      <div key={index} className={`centered ${item.color}`}>
+                          <div className='w-8 h-8 centered'>
+                              <span>{item.number}</span>
+                          </div>
+                      </div>
+                  )
+              })
+          }
+      </div>
+
+
+      <div className="grid grid-cols-4 h-40 w-40 font-bold text-black">
+          {
+              DivisionData.map((item,index) =>{
+                  return(
+                      <div key={index} className={`centered ${item.color}`}>
+                          <div className='w-8 h-8 centered'>
+                              <span>{item.alphabet}</span>
+                          </div>
+                      </div>
+                  )
+              })
+          }
+      </div>
+
+      <div className="grid grid-cols-4 h-40 w-40 font-bold text-white ">
+          {
+              DivisionData.map((item,index) =>{
+                  return(
+                      <div key={index} className={`centered ${item.color}`}>
+                          <div className='w-8 h-8 centered'>
+                              <span>{item.number}</span>
+                          </div>
+                      </div>
+                  )
+              })
+          }
+      </div>
+
+
+      <div className="grid grid-cols-4 h-40 w-40 font-bold text-black">
+          {
+              DivisionData.map((item,index) =>{
+                  return(
+                      <div key={index} className={`centered ${item.color}`}>
+                          <div className='w-8 h-8 centered'>
+                              <span>{item.alphabet}</span>
+                          </div>
+                      </div>
+                  )
+              })
+          }
+      </div>
+
+
+      <div className="grid grid-cols-4 h-40 w-40 font-bold text-white ">
+          {
+              DivisionData.map((item,index) =>{
+                  return(
+                      <div key={index} className={`centered ${item.color}`}>
+                          <div className='w-8 h-8 centered'>
+                              <span>{item.number}</span>
+                          </div>
+                      </div>
+                  )
+              })
+          }
+      </div>
+
+      <div className="grid grid-cols-4 h-40 w-40 font-bold text-black">
+          {
+              DivisionData.map((item,index) =>{
+                  return(
+                      <div key={index} className={`centered ${item.color}`}>
+                          <div className='w-8 h-8 centered'>
+                              <span>{item.alphabet}</span>
+                          </div>
+                      </div>
+                  )
+              })
+          }
+      </div>
+
+      
+      <div className="grid grid-cols-4 h-40 w-40 font-bold text-white ">
+          {
+              DivisionData.map((item,index) =>{
+                  return(
+                      <div key={index} className={`centered ${item.color}`}>
+                          <div className='w-8 h-8 centered'>
+                              <span>{item.number}</span>
+                          </div>
+                      </div>
+                  )
+              })
+          }
+      </div>
+
+
+      <div className="grid grid-cols-4 h-40 w-40 font-bold text-black">
+          {
+              DivisionData.map((item,index) =>{
+                  return(
+                      <div key={index} className={`centered ${item.color}`}>
+                          <div className='w-8 h-8 centered'>
+                              <span>{item.alphabet}</span>
+                          </div>
+                      </div>
+                  )
+              })
+          }
+      </div>
+
+      <div className="grid grid-cols-4 h-40 w-40 font-bold text-white ">
+          {
+              DivisionData.map((item,index) =>{
+                  return(
+                      <div key={index} className={`centered ${item.color}`}>
+                          <div className='w-8 h-8 centered'>
+                              <span>{item.number}</span>
+                          </div>
+                      </div>
+                  )
+              })
+          }
+      </div>
+
+
+      <div className="grid grid-cols-4 h-40 w-40 font-bold text-black">
+          {
+              DivisionData.map((item,index) =>{
+                  return(
+                      <div key={index} className={`centered ${item.color}`}>
+                          <div className='w-8 h-8 centered'>
+                              <span>{item.alphabet}</span>
+                          </div>
+                      </div>
+                  )
+              })
+          }
+      </div>
+
+
+    </section>
+  </React.Fragment>
+  )
+}
+
+export default MainPage
---
+++ nestedGrid/src/data/DivisionData.js
@@ -0,0 +1,100 @@
+export const DivisionData = [
+    {
+    
+      number:1,
+      alphabet:'a',
+      color:'bg-yellow-500'
+    },
+    {
+    
+      number:2,
+      alphabet:'b',
+      color:'bg-blue-900'
+    },
+    {
+    
+      number:3,
+      alphabet:'c',
+      color:'bg-red-900'
+    },
+    {
+    
+      number:4,
+      alphabet:'d',
+      color:'bg-yellow-200'
+    },
+    {
+    
+      number:5,
+      alphabet:'e',
+      color:'bg-green-900'
+    },
+    {
+    
+      number:6,
+      alphabet:'f',
+      color:'bg-blue-500'
+    },
+    {
+    
+      number:7,
+      alphabet:'g',
+      color:'bg-green-500'
+    },
+    {
+    
+      number:8,
+      alphabet:'h',
+      color:'bg-yellow-900'
+    },
+    {
+    
+      number:9,
+      alphabet:'i',
+      color:'bg-pink-200'
+    },
+    {
+    
+      number:10,
+      alphabet:'j',
+      color:'bg-red-600'
+    },
+    {
+    
+      number:11,
+      alphabet:'k',
+      color:'bg-red-400'
+    },
+    {
+    
+      number:12,
+      alphabet:'l',
+      color:'bg-indigo-300'
+    },
+    {
+    
+      number:13,
+      alphabet:'m',
+      color:'bg-indigo-400'
+    },
+    {
+    
+      number:14,
+      alphabet:'n',
+      color:'bg-purple-400'
+    },
+    {
+    
+      number:15,
+      alphabet:'o',
+      color:'bg-pink-800'
+    },
+    {
+    
+        number:16,
+        alphabet:'p',
+        color:'bg-pink-200'
+      },
+    
+  ];
+  
